da11f8730a6ef37363c00ef6576e2791eb5125ec
Pedro Miguel Gomes Guerrero <123781972+PedroMGGDev@users.noreply.github.com>
2024-11-27 17:22:32 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,23 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Referências aos botões e containers
-  const sign_in_btn = document.querySelector("#sign-in-btn");
-  const sign_up_btn = document.querySelector("#sign-up-btn");
-  const container = document.querySelector(".container");
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
-  // Alterar para modo de cadastro
-  sign_up_btn.addEventListener("click", () => {
-    container.classList.add("sign-up-mode");
-    document.querySelector(".sign-in-form").style.display = "none";
-    document.querySelector(".sign-up-form").style.display = "block";
-  });
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
 
-  // Alterar para modo de login
-  sign_in_btn.addEventListener("click", () => {
-    container.classList.remove("sign-up-mode");
-    document.querySelector(".sign-in-form").style.display = "block";
-    document.querySelector(".sign-up-form").style.display = "none";
-  });
-
-  // Configuração do Firebase
-  
-  
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
